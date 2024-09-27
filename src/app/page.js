0fcdd175f9f275994130b3e28b2d364c39207b1e
yeosang0508 <yeosang0508@gmail.com>
2024-09-27 16:39:15 +0900

    MUI 추가

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    hi
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="z-10 max-w-5xl w-full items-center justify-between text-sm lg:flex">
+          hi
+        </div>
+      </main>
     </>
   );
 }

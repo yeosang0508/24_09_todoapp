295086e8b8764fb7ed4c1be59a9690689846ec3b
yeosang0508 <yeosang0508@gmail.com>
2024-09-30 10:34:33 +0900

    button에 tailwind, mui 적용

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
-import Image from 'next/image';
+'use client';
+import Button from '@mui/material/Button';
 
 export default function Home() {
   return (
     <>
-      <div></div>
+      <Button className="bg-red-500" variant="contained">
+        Contained
+      </Button>
     </>
   );
 }

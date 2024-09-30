ddd7ca6efda80c9f14ebb654cc4544b30c60d77d
yeosang0508 <yeosang0508@gmail.com>
2024-09-30 10:24:00 +0900

    자동 정렬 플러그인 prettier 설정 및 관련 패키지 추가

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,9 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div className="z-10 max-w-5xl w-full items-center justify-between text-sm lg:flex">
-          hi
-        </div>
-      </main>
+      <div></div>
     </>
   );
 }
